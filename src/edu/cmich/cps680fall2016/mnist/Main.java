d7edc076b7f1f9bda7b61c55737726a81383c93b
JRoush <@>
2016-11-30 15:51:11 -0500

    Split main testing and training into dedicated functions

--- src/edu/cmich/cps680fall2016/mnist/Main.java
+++ src/edu/cmich/cps680fall2016/mnist/Main.java
@@ -7,64 +7,83 @@ import static edu.cmich.cps680fall2016.mnist.SimpleNN.*;
 
 public class Main {
 
-    public static LogWindow out = new LogWindow("Test Log!");
+    public static LogWindow out = new LogWindow("Output Log");
 
     public static void main(String[] args) throws IOException {
         SimpleNN nn = new SimpleNN(new int[] { 28 * 28, 300, 10 },
                 new Activation[] { LOGISTIC, LOGISTIC }, new Random());
 
+        out.println("Training ...");
+        train(nn, 10000);
+
+        test(nn, 20, true);
+
+        out.println("Testing ...");
+        float results = test(nn, 10000, false);
+        out.format("Test Errors: %6.2f%% incorrect\n", (1 - results) * 100F);
+
+        out.anyKeyToClose();
+    }
+
+    public static void train(SimpleNN nn, int count) throws IOException {
         float[][] act = nn.valueArray();
         float[][] err = nn.valueArray();
         float[] exp = new float[10];
+        float rate = 0.1F;
 
-        for (int rep = 1; rep > 0; rep--) {
-            ImageSet imgf = new ImageSet("data/train-images-idx3-ubyte.gz");
-            LabelSet lblf = new LabelSet("data/train-labels-idx1-ubyte.gz");
-            for (int i = 0; imgf.hasNextImage(); i++) {
-                if (i % 1000 == 0) out.println("Training on image " + i);
-                if (i > 10000) break;
-
-                byte label = lblf.nextLabel();
+        for (int c = 0; c < count; c++) {
+            ImageSet img = new ImageSet("data/train-images-idx3-ubyte.gz");
+            LabelSet lbl = new LabelSet("data/train-labels-idx1-ubyte.gz");
+            for (; img.hasNextImage() && lbl.hasNextLabel() && c < count; c++) {
+                if (c % 5000 == 0) out.format("Training image %8d ...\n", c);
+                //
+                byte label = lbl.nextLabel();
                 Arrays.fill(exp, 0);
                 exp[label] = 1;
-                //            out.println(label);
+                img.nextImage(act[0]);
+                //
+                nn.train(act, err, exp, rate);
+            }
+        }
+    }
 
-                imgf.nextImage(act[0]);
-                //            DispImage b = new DispImage(DispImage.floatPix(act[0]),
-                //                    imgf.rowCnt, imgf.colCnt);
-                //            out.println(b.scaled(8));
+    public static float test(SimpleNN nn, int count, boolean print)
+            throws IOException {
+        float[][] act = nn.valueArray();
+        int correctcnt = 0;
 
-                nn.train(act, err, exp, 0.1F);
-                //            out.println(new DispImage(DispImage.floatPix(vals[1]), 2, 15).scaled(8));
-                //            out.println(new DispImage(DispImage.floatPix(vals[1]), 1, 10).scaled(8));
+        for (int c = 0; c < count; c++) {
+            ImageSet img = new ImageSet("data/t10k-images-idx3-ubyte.gz");
+            LabelSet lbl = new LabelSet("data/t10k-labels-idx1-ubyte.gz");
+            for (; img.hasNextImage() && lbl.hasNextLabel() && c < count; c++) {
+                if (c % 5000 == 0) out.format("Testing image %8d ...\n", c);
+                //
+                byte label = lbl.nextLabel();
+                img.nextImage(act[0]);
+                //
+                nn.apply(act);
+                float[] output = act[act.length - 1];
+                int answer = label;
+                for (int i = 0; i < output.length; i++) {
+                    if (output[i] >= output[answer]) answer = i;
+                }
+                if (answer == label) correctcnt++;
+                //
+                if (print) {
+                    out.println(label);
+                    for (float[] a : act) {
+                        int h = (a.length + img.colCnt - 1) / img.colCnt;
+                        int w = a.length / h;
+                        DispImage d = new DispImage(DispImage.floatPix(a), h, w);
+                        out.println(d.scaled(6));
+                    }
+                    out.print("Classifier result: " + answer);
+                    if (answer != label) out.print(" !! EXPECTED " + label);
+                    out.println();
+                }
             }
         }
 
-        //        out.println("Done training ...");
-        //        for (int w = 0; w < act.length - 1; w++) {
-        //            out.println(nn.weightImage(w));
-        //        }
-
-        ImageSet imgt = new ImageSet("data/t10k-images-idx3-ubyte.gz");
-        LabelSet lblt = new LabelSet("data/t10k-labels-idx1-ubyte.gz");
-        int testcnt = 0, correct = 0;
-        for (; imgt.hasNextImage(); testcnt++) {
-            if (testcnt % 1000 == 0)
-                out.println("Testing on image " + testcnt);
-            byte label = lblt.nextLabel();
-            imgt.nextImage(act[0]);
-            nn.apply(act);
-            boolean c = true;
-            for (int i = 0; i < 10; i++) {
-                if (i == label) continue;
-                if (act[act.length - 1][i] >= act[act.length - 1][label])
-                    c = false;
-            }
-            if (c) correct++;
-        }
-        out.format("Test Results: %6.2f%% correct\n", (correct * 100F)
-                / testcnt);
-
-        out.anyKeyToClose();
+        return correctcnt / (float) count;
     }
 }
